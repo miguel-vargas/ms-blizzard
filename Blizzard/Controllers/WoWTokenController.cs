10256a5c2968e81b8deb4c2dae3d84f355275544
Miguel <migs.a.vargas@gmail.com>
2020-05-02 20:24:37 -0400

    Removed default controller. Added WoW Token Controller. Defined a GET that has a required parameter of a region. GET simply returns the parameter supplied for now.

---
+++ Blizzard/Controllers/WoWTokenController.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MigsTech.Blizzard.Controllers
+{
+    /// <summary>
+    /// The controller for getting WoW Tokens.
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
+    [ApiExplorerSettings(GroupName = "WoW Token")]
+    public class WoWTokenController : ControllerBase
+    {
+        private readonly ILogger<WoWTokenController> logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WoWTokenController"/> class.
+        /// </summary>
+        /// <param name="logger">The logger</param>
+        public WoWTokenController(ILogger<WoWTokenController> logger)
+        {
+            this.logger = logger;
+        }
+
+        /// <summary>
+        /// Gets the WoW Token for the specified region.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("{region}", Name = "Get WoW Token")]
+        public async Task<string> Get([FromRoute][Required] string region)
+        {
+            return await Task.FromResult(region);
+        }
+    }
+}

e1733f8b59cb0ab67db47e62311736f8019474f8
Miguel <migs.a.vargas@gmail.com>
2020-05-03 13:46:28 -0400

    Added WoWTokenController unit tests.

---
+++ Blizzard.UnitTests/Controllers/WoWTokenControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Logging;
+using MigsTech.Blizzard.Controllers;
+using Moq;
+using Xunit;
+
+namespace MigsTech.Blizzard.UnitTests.Controllers
+{
+    public class WoWTokenControllerTests
+    {
+        private readonly WoWTokenController controller;
+
+        public WoWTokenControllerTests()
+        {
+            var loggerMock = new Mock<ILogger<WoWTokenController>>();
+            this.controller = new WoWTokenController(loggerMock.Object);
+        }
+
+        [Fact]
+        public void WoWTokenController_Get_ReturnsTypeString()
+        {
+            // Arrange
+            const string testRegion = "test";
+
+            // Act
+            var response = this.controller.Get(testRegion);
+
+            // Assert
+            Assert.IsAssignableFrom<string>(response.Result);
+        }
+
+        [Fact]
+        public void WoWTokenController_Get_ReturnsGivenString()
+        {
+            // Arrange
+            const string testRegion = "test";
+
+            // Act
+            var response = this.controller.Get(testRegion);
+
+            // Assert
+            Assert.Equal(testRegion, response.Result);
+        }
+    }
+}

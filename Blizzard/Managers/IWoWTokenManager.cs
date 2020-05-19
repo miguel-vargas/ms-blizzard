df5f7b5bce7865a0a9600279b6ecff07b214d1db
Miguel <migs.a.vargas@gmail.com>
2020-05-19 14:05:56 -0400

    Add WoW token manager interface and implementation that will handle the business logic of getting a WoW token.

---
+++ Blizzard/Managers/IWoWTokenManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace MigsTech.Blizzard.Managers
+{
+    /// <summary>
+    /// An interface for the WoW Token manager.
+    /// </summary>
+    public interface IWoWTokenManager
+    {
+        /// <summary>
+        /// Gets the WoW Token for the specified region.
+        /// </summary>
+        /// <param name="region">The region.</param>
+        /// <returns></returns>
+        Task<string> GetWoWTokenByRegion(string region);
+    }
+}

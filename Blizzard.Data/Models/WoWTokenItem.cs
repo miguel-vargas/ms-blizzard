eefea139275254f9872b58bb7ce5370f9cba7481
Miguel <migs.a.vargas@gmail.com>
2020-05-20 15:21:58 -0400

    Added new models for the data responses and an enum for the available regions. Removed the Blizzard API model as I don't want any of those living in a class instance memory.

---
+++ Blizzard.Data/Models/WoWTokenItem.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace MigsTech.Blizzard.Data.Models
+{
+    /// <summary>
+    /// Represents a wow token item.
+    /// </summary>
+    [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
+    public class WoWTokenItem
+    {
+        #region Fields and Properties
+        /// <summary>
+        /// Gets or sets the data identifier.
+        /// </summary>
+        public string LastUpdatedTimestamp { get; set; }
+
+        /// <summary>
+        /// Gets or sets the revision number.
+        /// </summary>
+        public string Price { get; set; }
+
+        /// <summary>
+        /// Gets or sets the region of this wow token.
+        /// </summary>
+        public string Region { get; set; }
+        #endregion
+    }
+}
+

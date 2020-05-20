6223197bef2d057dd4da3c3efa3a2f38ff22f76d
Miguel <migs.a.vargas@gmail.com>
2020-05-19 21:05:12 -0400

    Remove unused imports.

--- Blizzard.BusinessLogic/Managers/WoWTokenManager.cs
+++ Blizzard.BusinessLogic/Managers/WoWTokenManager.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using MigsTech.Blizzard.Data.Models;
 using MigsTech.Blizzard.Data.Services;
-using Newtonsoft.Json.Linq;
 
 namespace MigsTech.Blizzard.BusinessLogic.Managers
 {

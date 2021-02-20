919648dad044e18817f4ab170efd357ac0e84344
Miguel <migs.a.vargas@gmail.com>
2021-02-20 15:07:23 -0500

    Add model for blizzard oauth settings.

---
+++ Blizzard.BusinessLogic/Models/BlizzardOAuthSettings.cs
@@ -0,0 +1,25 @@
+﻿namespace MigsTech.Blizzard.BusinessLogic.Models
+{
+    /// <summary>
+    /// Represents settings to authenticate against Blizzard's API
+    /// </summary>
+    public class BlizzardOAuthSettings
+    {
+        internal const string BlizzardSettingsKey = "Blizzard";
+
+        /// <summary>
+        /// Blizzard's Auth Endpoint.
+        /// </summary>
+        public string AuthUri { get; set; }
+
+        /// <summary>
+        /// Client Id.
+        /// </summary>
+        public string ClientId { get; set; }
+
+        /// <summary>
+        /// Client Secret.
+        /// </summary>
+        public string ClientSecret { get; set; }
+    }
+}
